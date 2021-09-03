f46a8b169b49554609b25f64b130885eb0ac9b5d
guipeeix7 <guizinpeix7@gmail.com>
2021-09-03 14:46:17 -0300

    fixed layout bug

--- WebApp/resources/views/welcome.blade.php
+++ WebApp/resources/views/welcome.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('unlogged.layout.app')
 
-        <title>Funcionou :D</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
+@section('content')
         <style>
             html, body {
                 background-color: #fff;
@@ -65,20 +56,6 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     Busca Imposto
@@ -98,5 +75,4 @@
                 </div>
             </div>
         </div>
-    </body>
-</html>
+        @endsection
--- WebApp/routes/web.php
+++ WebApp/routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home',
+Route::get('/home2',
     [Home::class, 'index']
 );
 
