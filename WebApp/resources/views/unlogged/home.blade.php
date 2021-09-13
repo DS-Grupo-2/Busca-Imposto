1c4c9779c0ca4aa2dbc7d4d8039d33488a81be6c
Luca <lucaverdade@gmail.com>
2021-09-13 18:27:33 -0300

    changed home

--- WebApp/resources/views/unlogged/home.blade.php
+++ WebApp/resources/views/unlogged/home.blade.php
@@ -5,39 +5,47 @@
     <div class="container">
         <div class="row">
             <div class="title-area">
-                <h2>Our Services</h2>
+                <h2>Nossos serviços</h2>
                 <div class="separator separator-danger">✻</div>
-                <p class="description">We promise you a new look and more importantly, a new attitude. We build that by getting to know you, your needs and creating the best looking clothes.</p>
+                <p class="description"></p>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4">
-                <div class="info-icon">
-                    <div class="icon text-danger">
-                        <i class="pe-7s-graph1"></i>
-                    </div>
-                    <h3>Sales</h3>
-                    <p class="description">We make our design perfect for you. Our adjustment turn our clothes into your clothes.</p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="info-icon">
-                    <div class="icon text-danger">
-                        <i class="pe-7s-note2"></i>
-                    </div>
-                    <h3>Content</h3>
-                    <p class="description">We create a persona regarding the multiple wardrobe accessories that we provide..</p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="info-icon">
-                    <div class="icon text-danger">
-                        <i class="pe-7s-music"></i>
-                    </div>
-                    <h3>Music</h3>
-                    <p class="description">We like to present the world with our work, so we make sure we spread the word regarding our clothes.</p>
-                </div>
-            </div>
+        <div class="card col-md-4 m-2" style="width: 18rem; ">
+  <img class="card-img-top" src="assetsunlogged/img/new_logo.png" alt="Card image cap">
+  <div class="card-body">
+  <a href="">   <h5 class="card-title">Produto</h5>
+    </a>
+    <p class="card-text">$PREÇO$.</p>
+    
+  </div>
+</div>
+<div class="card col-md-4" style="width: 18rem; ">
+  <img class="card-img-top" src="assetsunlogged/img/new_logo.png" alt="Card image cap">
+  <div class="card-body">
+  <a href="">   <h5 class="card-title">Produto</h5>
+    </a>
+    <p class="card-text">$PREÇO$.</p>
+
+  </div>
+</div>
+             <div class="card col-md-4" style="width: 18rem; ">
+  <img class="card-img-top" src="assetsunlogged/img/new_logo.png" alt="Card image cap">
+  <div class="card-body">
+  <a href="">   <h5 class="card-title">Produto</h5>
+    </a>
+    <p class="card-text">$PREÇO$.</p>
+  </div>
+</div>
+<div class="card col-md-4" style="width: 18rem; ">
+  <img class="card-img-top" src="assetsunlogged/img/new_logo.png" alt="Card image cap">
+  <div class="card-body">
+  <a href="">   <h5 class="card-title">Produto</h5>
+    </a>
+    <p class="card-text">$PREÇO$.</p>
+  
+  </div>
+</div>
         </div>
     </div>
 </div>
