bd81fa0391e526636a8d7ebc8109551cf02f2190
jlmds <202016604@aluno.unb.br>
2021-09-13 15:09:47 -0300

    changed categories and products

--- WebApp/resources/views/logged/Categories/view.blade.php
+++ WebApp/resources/views/logged/Categories/view.blade.php
@@ -1,6 +1,7 @@
 @extends('logged.base.app')
 @section('content')
 
+<div class="p-3">
     <form method="POST" action="{{ route('categories-create') }}">
         @csrf
         <label for="email">
@@ -14,19 +15,47 @@
             </span>
         @enderror
 
-        <button type="submit"> Salvar </button>
+        <button type="submit" class="btn btn-primary"> Salvar </button>
     </form>
     <hr>
-    @foreach($list as $item)
-        <p>This is category {{ $item->id }}</p>
-        <p>NomeCategoria {{ $item->NomeCategoria }}</p>
+
+    
+    <div class="table-responsive">
+            <table class="table table-hover">
+             <thead>
+               <tr>
+                 <th scope="col">ID</th>
+                 <th scope="col">Categoria</th>
+                 <th scope="col">Criado</th>
+                 <th scope="col">Atualizado</th>
+                 <th scope="col"></th>
+               </tr>
+             </thead>
+             @foreach($list as $item)         
+             <tbody>
+                <tr> 
+                 <th scope="row">{{ $item->id }}</th>
+                    <td>{{ $item->NomeCategoria }}</td>
+                    <td>{{ $item->created_at }}</td>
+                    <td>{{ $item->updated_at }}</td>
+                    <td scope="col">
+                    <button type="submit" class="btn btn-danger float-right"><a href="{{ url('/system/categories/delete/'.$item->id) }}" class="text-white"> Deletar</a></button> 
+                    <button type="submit" class="btn btn-success float-right"><a href="{{ url('/system/categories/edit/'.$item->id) }} " class="text-white" > Editar </a></button></td>
+                </tr>        
+                </tbody>
+             @endforeach  {{ $list->links() }}
+           </table>
+    </div>    
+</div>
+        {{-- <p>This is category{{ $item->id }} </p>
+        <p>NomeCategoria {{ $item->NomeCategoria }}</p> 
         <p>created_at {{ $item->created_at }}</p>
         <p>updated_at {{ $item->updated_at }}</p>
-        <a href="{{ url('/system/categories/edit/'.$item->id) }} " > editar </a>
-        <a href="{{ url('/system/categories/delete/'.$item->id) }}" > deletar</a>
+        <button type="submit" class="btn btn-success"><a href="{{ url('/system/categories/edit/'.$item->id) }} " class="text-white" > Editar </a></button>
+        <button type="submit" class="btn btn-danger"><a href="{{ url('/system/categories/delete/'.$item->id) }}" class="text-white"> Deletar</a></button>  --}}
 
-    @endforeach
-    {{ $list->links() }}
+    
+   
 @endsection
 
 
